a142135a91369666fdeb62a8d205c0ee544a8d37
Salud Lemus <slemu003@ucr.edu>
2018-01-03 21:40:40 -0800

    Discarded user prompt and moved it to parser class

--- run.cpp
+++ run.cpp
@@ -7,69 +7,26 @@
 
 #include <iostream>
 #include <string>
-#include <unistd.h>
-#include <cstdlib>
-#include <sys/types.h>
-#include <pwd.h>
-#include <stdio.h>
 #include <errno.h>
+#include <stdio.h>
 #include "Parser.h"
 using namespace std;
 
-void checkUserFailure();
-
 int main() {
-	uid_t user_id = getuid(); // get current user's ID
-	errno = 0;
-	struct passwd* user_info = getpwuid(user_id); // retrieve user ID's info
-	string user_input;
-
-	if (user_info == NULL) { // entry not found
-		checkUserFailure();
-		exit(EXIT_FAILURE);
-	}
-
 	while (true) {
-		while(user_input.size() == 0) {
-			cout << user_info->pw_name << "$ " << flush; // prompt
-			getline(cin, user_input); // ask for user input
-		}
+		Parser new_parse;
 
-		Parser new_parse(user_input);
+		new_parse.askUser(); // prompt user and get input
 
-		new_parse.parse();
+		new_parse.parse(); // parse and build commands
 
 		AllCommands* new_cmd = new AllCommands(new_parse.getCommands());
 
 		new_cmd->execute(); // execute cmds
 
 		delete new_cmd; // dealloc. cmd
-		user_input = ""; // reset user's input
+
+		new_parse.setUserInput(""); // start over
 	}
 	return 0;
-}
-
-void checkUserFailure() {
-	if (errno == 0 || errno == ENOENT || errno == EBADF || errno == EPERM) {
-		perror("Either the name or uid was not found.");
-	}
-	else if (errno == EINTR) {
-		perror("A signal was caught.");
-	}
-	else if (errno == EIO) {
-			perror("I/O error.");
-	}
-	else if (errno == EMFILE) {
-			perror("The maximum number of files have already been opened within the current calling process.");
-	}
-	else if (errno == ENOMEM) {
-			perror("Insufficient memory to allocate passwd structure.");
-	}
-	else if (errno == ERANGE) {
-			perror("Insufficient buffer space supplied.");
-	}
-	else if (errno == ENFILE) {
-			perror("The maximum number of files was open already in the system.");
-	}
-	return;
 }
